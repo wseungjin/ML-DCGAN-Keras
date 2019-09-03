38fdc77a3c56afe826da0c116adb2a4f2ad77d0e
wseungjin <bose1021@naver.com>
2019-09-03 10:28:02 +0900

    update

--- networks.py
+++ networks.py
@@ -7,8 +7,7 @@ from keras.layers import *
 from keras.optimizers import * 
 import keras.backend as K 
 import matplotlib.pyplot as plt
-from tensorflow.contrib.data import prefetch_to_device, shuffle_and_repeat, map_and_batch
-
+from PIL import Image
 
 class DCGAN:
     def __init__(self,args):
@@ -45,10 +44,9 @@ class DCGAN:
         self.test_num = args.test_num
         
         self.c_dim = 3
-        self.data = load_data(dataset_name=self.dataset_name)
         self.custom_dataset = True
 
-        self.dataset_num = len(self.data)
+        # self.dataset_num = len(self.data)
         
         self.sample_dir = os.path.join(self.sample_dir, self.model_dir)
         check_folder(self.sample_dir)
@@ -60,7 +58,7 @@ class DCGAN:
 
         print("##### Information #####")
         print("# dataset : ", self.dataset_name)
-        print("# dataset number : ", self.dataset_num)
+        # print("# dataset number : ", self.dataset_num)
         print("# batch_size : ", self.batch_size)
         print("# img_size : ", self.img_size)
         print("# epoch : ", self.epoch)
@@ -77,43 +75,68 @@ class DCGAN:
         
     def generator(self):
         g_model = Sequential()
+        dropout = 0.5
         
-        g_model.add(Conv2D(self.ch*2,kernel_size=(3,3),strides=1,padding='same',input_shape=self.input_shape))
+        g_model.add(Dense(8 * 8 * 256, input_shape=(self.z_dim,)))
+        
+        g_model.add(BatchNormalization(momentum=0.8))
+        g_model.add(ReLU())
+        g_model.add(Reshape((8, 8, 256)))
+        g_model.add(Dropout(dropout))
+
+        g_model.add(Dropout(dropout))
+        g_model.add(UpSampling2D())
+        
+        g_model.add(Conv2DTranspose(128, 5, padding='same'))
         g_model.add(BatchNormalization())
         g_model.add(ReLU())
+        g_model.add(Dropout(dropout))
         
-        g_model.add(Conv2D(self.ch*2,kernel_size=(3,3),strides=2,padding='same'))
+        g_model.add(UpSampling2D())
+        
+        g_model.add(Conv2DTranspose(64, 5, padding='same'))
+        g_model.add(BatchNormalization(momentum=0.9))
+        g_model.add(Activation('relu'))
+        g_model.add(Dropout(dropout))
+
+        g_model.add(UpSampling2D())
+        
+        g_model.add(Conv2DTranspose(32, 5, padding='same'))
         g_model.add(BatchNormalization())
         g_model.add(ReLU())
+        g_model.add(Dropout(dropout))
+
+        # g_model.add(UpSampling2D())
         
-        g_model.add(Conv2D(self.ch//2,kernel_size=(3,3),strides=2,padding='same'))
-        g_model.add(BatchNormalization())
-        g_model.add(ReLU())
-        
-        g_model.add(Conv2D(self.ch//2,kernel_size=(3,3),strides=2,padding='same'))
-        g_model.add(BatchNormalization())
-        g_model.add(ReLU())
+        g_model.add(Conv2DTranspose(self.c_dim, 5, padding='same')) 
+        g_model.add(Activation('sigmoid'))
+
         
         return g_model
 
     def discriminator(self):
         d_model=Sequential()
+        dropout = 0.5
         
         d_model.add(Conv2D(self.ch*2,kernel_size=(3,3),strides=1,padding='same',input_shape=self.input_shape))
         d_model.add(BatchNormalization())
         d_model.add(ReLU())
+        d_model.add(Dropout(dropout))
         
         d_model.add(Conv2D(self.ch*2,kernel_size=(3,3),strides=2,padding='same'))
         d_model.add(BatchNormalization())
         d_model.add(ReLU())
+        d_model.add(Dropout(dropout))
         
         d_model.add(Conv2D(self.ch//2,kernel_size=(3,3),strides=2,padding='same'))
         d_model.add(BatchNormalization())
         d_model.add(ReLU())
+        d_model.add(Dropout(dropout))
         
         d_model.add(Conv2D(self.ch//2,kernel_size=(3,3),strides=2,padding='same'))
         d_model.add(BatchNormalization())
         d_model.add(ReLU())
+        d_model.add(Dropout(dropout))
         
         d_model.add(Flatten())
         d_model.add(Dense(1))
@@ -125,22 +148,16 @@ class DCGAN:
     def build_model(self):
         
         Image_Data_Class = ImageData(self.img_size, self.c_dim)
-        inputs = tf.data.Dataset.from_tensor_slices(self.data)
+        self.data = Image_Data_Class.load_data(dataset_name=self.dataset_name)
 
-        gpu_device = '/gpu:0'
-        self.inputs = inputs.\
-            apply(shuffle_and_repeat(self.dataset_num)).\
-            apply(map_and_batch(Image_Data_Class.image_processing, self.batch_size, num_parallel_batches=16, drop_remainder=True)).\
-            apply(prefetch_to_device(gpu_device, self.batch_size))
-            
-        numpy_datas=self.inputs.numpy()    
-            
-        print(numpy_datas.shape)    
-        self.input_shape=self.inputs.shape
-                
+        
+        self.input_shape=(64,64,3)
+                        
         
         self.g_model=self.generator()
+        self.g_model.summary()
         self.d_model=self.discriminator()
+        self.d_model.summary()
         
         self.d_optimizer = Adam(lr=self.d_learning_rate)
         self.d_model.compile(self.d_optimizer,loss="binary_crossentropy")
@@ -149,15 +166,18 @@ class DCGAN:
         self.g_model.compile("SGD",loss="binary_crossentropy")
 
         self.d_model.trainable = False
-        self.g_d_model = Sequential
+        self.g_d_model = Sequential()
         self.g_d_model.add(self.g_model)
         self.g_d_model.add(self.d_model)
+        self.g_d_model.summary()
+        
         self.g_d_model.compile(self.g_optimizer,loss="binary_crossentropy")
 
 
 
     def train(self):
-        x_train = self.inputs
+        x_train = self.data
+        print(x_train.shape)
         BATCH_SIZE=self.batch_size
         
     
@@ -167,14 +187,14 @@ class DCGAN:
             print("Epoch is", epoch)
             print("Number of batches", int(x_train.shape[0]/BATCH_SIZE))
             for index in range(int(x_train.shape[0]/BATCH_SIZE)):
-                noise = np.random.uniform(-1, 1, size=(BATCH_SIZE, 100))
+                noise = np.random.uniform(-1, 1, size=(BATCH_SIZE, 128))
                 image_batch = x_train[index*BATCH_SIZE:(index+1)*BATCH_SIZE]
                 generated_images = self.g_model.predict(noise, verbose=0)
                 X = np.concatenate((image_batch, generated_images))
                 y = [1] * BATCH_SIZE + [0] * BATCH_SIZE
                 self.d_loss = self.d_model.train_on_batch(X, y)
                 print("batch %d d_loss : %f" % (index, self.d_loss))
-                noise = np.random.uniform(-1, 1, (BATCH_SIZE, 100))
+                noise = np.random.uniform(-1, 1, (BATCH_SIZE, 128))
                 self.d_model.trainable = False
                 self.g_loss = self.g_d_model.train_on_batch(noise, [1] * BATCH_SIZE)
                 self.d_model.trainable = True
@@ -182,9 +202,19 @@ class DCGAN:
                 if epoch % 10 == 0:
                     self.g_model.save_weights('generator', True)
                     self.d_model.save_weights('discriminator', True)
+                    
+        self.phase='test'
 
 
         
         
-    # def test(self):
-        
+    def test(self):
+        g = self.generator()
+        g.compile(loss='binary_crossentropy', optimizer="SGD")
+        g.load_weights('generator')
+        for i in range(20):
+            noise = np.random.uniform(-1, 1, (self.batch_size, 128))
+            generated_images = g.predict(noise, verbose=1)
+            image = image*255.0
+            Image.fromarray(image.astype(np.uint8)).save("generated_image_"+str(i) +".png")
+            
--- utills.py
+++ utills.py
@@ -4,6 +4,8 @@ import random, os
 from glob import glob
 from tensorflow.contrib import slim
 import cv2
+from random import shuffle # mixing up or currently ordered data that might lead our network astray in training.
+# from tqdm import tqdm 
 
 
 class ImageData:
@@ -14,13 +16,36 @@ class ImageData:
         #rgb 3
         self.channels = channels
 
-    def image_processing(self, filename):
-        x = tf.read_file(filename)
-        x_decode = tf.image.decode_jpeg(x, channels=self.channels, dct_method='INTEGER_ACCURATE')
-        img = tf.image.resize_images(x_decode, [self.img_size, self.img_size])
-        img = tf.cast(img, tf.float32) / 127.5 - 1
+    # def image_processing(self, filename):
+    #     x = tf.read_file(filename)
+    #     x_decode = tf.image.decode_jpeg(x, channels=self.channels, dct_method='INTEGER_ACCURATE')
+    #     img = tf.image.resize_images(x_decode, [self.img_size, self.img_size])
+    #     img = tf.cast(img, tf.float32) / 127.5 - 1
 
-        return img
+    #     return img
+    def load_data(self,dataset_name,flag=0):
+        if flag==0:
+            TRAIN_DIR = os.path.join("./dataset", dataset_name)
+            print(TRAIN_DIR)
+            training_data = []
+            for img in (os.listdir(TRAIN_DIR)):
+                # print(img)
+                path = os.path.join(TRAIN_DIR,img)
+                # print(path)
+                img = cv2.imread(path,cv2.IMREAD_COLOR)
+                # print(img)
+                img = cv2.resize(img, (self.img_size,self.img_size))
+                training_data.append([np.array(img).astype('float32')])
+            shuffle(training_data)
+            x_train = np.vstack(training_data) / 255.0
+            np.save(str(dataset_name) + '_train_data.npy', x_train)
+            print(x_train.shape)
+        else:
+            x_train=np.load(str(dataset_name)+'_train_data.npy')
+
+            
+    
+        return x_train
 
 
 def check_folder(log_dir):
@@ -29,6 +54,7 @@ def check_folder(log_dir):
     return log_dir
 
 
-def load_data(dataset_name) :
-    x = glob(os.path.join("./dataset", dataset_name, '*.*'))
-    return x
+# def load_data(dataset_name) :
+#     x = glob(os.path.join("./dataset", dataset_name, '*.*'))
+#     return x
+
